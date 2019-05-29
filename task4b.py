f937c73e8093e8d8f7804d2c98b4e1f2d11ac697
nispk <49596492+nispk@users.noreply.github.com>
2019-05-29 09:45:41 +0200

    Add files via upload

--- task4b.py
+++ task4b.py
@@ -21,7 +21,8 @@ args = parse_arguments()
 out_file = "Practise/task3b_output"
 err_file =  "Practise/task3b_error"
 
-os.system("touch"+out_file)
+os.system("touch "+out_file)
+os.system("touch "+err_file)
 inputs = [os.fdopen(int(args.inputs[0]), 'rb')]
 x = np.fromfile(inputs[0],dtype=np.float16,count=5)
 
@@ -30,10 +31,18 @@ print ("from3b",x)
 x = x/0
 w = np.mean(x)
 print (w)
+
+#file = open(out_file, 'a')
+#sys.stdout = file
+#file.close
+#file = open(err_file, 'a')
+#sys.stderr = file
+#file.close
 #sys.stderr =  os.mkfifo(err_file) 
 #sys.stdout = os.mkfifo(out_file)
-#sys.stdout = open(out_file,'w') 
-#sys.stderr = open(err_file,'w')
+
+sys.stdout = open(out_file,'w') 
+sys.stderr = open(err_file,'w')
 
 sys.stdout.flush()
 
